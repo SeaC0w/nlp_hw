bd0b5e0b74c1c6405238ffbe67c07f6e40bb2795
seac0w <kerim.celik@yahoo.com>
2017-10-09 16:50:33 -0500

    First commit, two homeworks in

---
+++ hw2_nlp/instal.py
@@ -0,0 +1,258 @@
+import nltk
+import re
+import math
+from tabulate import tabulate
+
+# mergeSort code taken from interactivepython.org
+def mergeSort(alist):
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i][1] > righthalf[j][1]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+
+# creates a tokenizer to tokenize by sentence
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+f = open("HW2_InputFile.txt", 'r')
+fil = f.read()
+
+# removes the period from title prefixes to prevent incorrect sentence splits
+fil = re.sub('Dr.', 'Dr', fil)
+fil = re.sub('Mr.', 'Mr', fil)
+fil = re.sub('Mrs.', 'Mrs', fil)
+fil = re.sub('Ms.', 'Ms', fil)
+
+# get sentence tokens and word tokens
+sent_tok = tokenizer.tokenize(fil)
+tokens = nltk.word_tokenize(fil)
+
+# tell the user the number of sentences and words in the file
+num_sent = len(sent_tok)
+num_word = len(tokens)
+print('Number of sentences in file: ' + str(num_sent))
+print('Number of words in file: ' + str(num_word))
+
+# sort the words in file by number of occurences
+# then print as a table
+added = []
+tabl = []
+for item in tokens:
+    if item not in added:
+        tabl.append([item, 1])
+        added.append(item)
+    else:
+        for row in tabl:
+            if row[0] == item:
+                row[1] += 1
+mergeSort(tabl)
+print(tabulate(tabl, headers=['Word', 'Occurences']))
+
+# split the corpus into 90/10, 80/20, and 70/30 train/test splits
+sent90 = sent_tok[0:math.floor(0.9*len(sent_tok))]
+test10 = sent_tok[math.floor(0.9*len(sent_tok)):-1]
+sent80 = sent_tok[0:math.floor(0.8*len(sent_tok))]
+test20 = sent_tok[math.floor(0.8*len(sent_tok)):-1]
+sent70 = sent_tok[0:math.floor(0.7*len(sent_tok))]
+test30 = sent_tok[math.floor(0.7*len(sent_tok)):-1]
+
+# POS tag the words in the sentences, then append the tags to a single list for
+# each train/test split
+pos90 = []
+pos80 = []
+pos70 = []
+for item in sent90:
+    pairs = nltk.pos_tag(nltk.word_tokenize(item))
+    for pair in pairs:
+        pos90.append(pair[1])
+
+for item in sent80:
+    pairs = nltk.pos_tag(nltk.word_tokenize(item))
+    for pair in pairs:
+        pos80.append(pair[1])
+
+for item in sent70:
+    pairs = nltk.pos_tag(nltk.word_tokenize(item))
+    for pair in pairs:
+        pos70.append(pair[1])
+
+# creates dictionaries to store the pos_tag n-grams with
+# their associated occurences
+# no need to create sentence end tag for bigram and trigram models as
+# the period of the sentences already acts as a sentence end tag
+unigram90 = dict()
+bigram90 = dict()
+trigram90 = dict()
+for tag in pos90:
+    if tag not in unigram90.keys():
+        unigram90[tag] = 1
+    else:
+        unigram90[tag] += 1
+
+prevtag = 'START'
+for tag in pos90:
+    if (prevtag, tag) not in bigram90.keys():
+        bigram90[(prevtag, tag)] = 1
+    else:
+        bigram90[(prevtag, tag)] += 1
+    prevtag = tag
+
+preprevtag = 'START'
+prevtag = 'START'
+for tag in pos90:
+    if (preprevtag, prevtag, tag) not in trigram90.keys():
+        trigram90[(preprevtag, prevtag, tag)] = 1
+    else:
+        trigram90[(preprevtag, prevtag, tag)] += 1
+    preprevtag = prevtag
+    prevtag = tag
+
+# gets the total number of n-grams for 90/10 split
+uni90total = 0
+bi90total = 0
+tri90total = 0
+for val in unigram90.values():
+    uni90total += val
+for val in bigram90.values():
+    bi90total += val
+for val in trigram90.values():
+    tri90total += val
+
+# calculates the probability of each n-gram for 90/10 split
+for key in unigram90.keys():
+    unigram90[key] = unigram90[key] / uni90total
+
+for key in bigram90.keys():
+    bigram90[key] = bigram90[key] / bi90total
+
+for key in trigram90.keys():
+    trigram90[key] = trigram90[key] / tri90total
+
+# repeat above process for other train/test split ratios
+# 80/20 train test split probability calculations
+unigram80 = dict()
+bigram80 = dict()
+trigram80 = dict()
+for tag in pos80:
+    if tag not in unigram80.keys():
+        unigram80[tag] = 1
+    else:
+        unigram80[tag] += 1
+
+prevtag = 'START'
+for tag in pos80:
+    if (prevtag, tag) not in bigram80.keys():
+        bigram80[(prevtag, tag)] = 1
+    else:
+        bigram80[(prevtag, tag)] += 1
+    prevtag = tag
+
+preprevtag = 'START'
+prevtag = 'START'
+for tag in pos80:
+    if (preprevtag, prevtag, tag) not in trigram80.keys():
+        trigram80[(preprevtag, prevtag, tag)] = 1
+    else:
+        trigram80[(preprevtag, prevtag, tag)] += 1
+    preprevtag = prevtag
+    prevtag = tag
+
+# gets the total number of n-grams for 80/20 split
+uni80total = 0
+bi80total = 0
+tri80total = 0
+for val in unigram80.values():
+    uni80total += val
+for val in bigram80.values():
+    bi80total += val
+for val in trigram80.values():
+    tri80total += val
+
+# calculates the probability of each n-gram for 80/20 split
+for key in unigram80.keys():
+    unigram80[key] = unigram80[key] / uni80total
+
+for key in bigram80.keys():
+    bigram80[key] = bigram80[key] / bi80total
+
+for key in trigram80.keys():
+    trigram80[key] = trigram80[key] / tri80total
+
+# 70/30 train test split probability calculations
+unigram70 = dict()
+bigram70 = dict()
+trigram70 = dict()
+for tag in pos70:
+    if tag not in unigram70.keys():
+        unigram70[tag] = 1
+    else:
+        unigram70[tag] += 1
+
+prevtag = 'START'
+for tag in pos70:
+    if (prevtag, tag) not in bigram70.keys():
+        bigram70[(prevtag, tag)] = 1
+    else:
+        bigram70[(prevtag, tag)] += 1
+    prevtag = tag
+
+preprevtag = 'START'
+prevtag = 'START'
+for tag in pos70:
+    if (preprevtag, prevtag, tag) not in trigram70.keys():
+        trigram70[(preprevtag, prevtag, tag)] = 1
+    else:
+        trigram70[(preprevtag, prevtag, tag)] += 1
+    preprevtag = prevtag
+    prevtag = tag
+
+# gets the total number of n-grams for 70/30 split
+uni70total = 0
+bi70total = 0
+tri70total = 0
+for val in unigram70.values():
+    uni70total += val
+for val in bigram70.values():
+    bi70total += val
+for val in trigram70.values():
+    tri70total += val
+
+# calculates the probability of each n-gram for 70/30 split
+for key in unigram70.keys():
+    unigram70[key] = unigram70[key] / uni70total
+
+for key in bigram70.keys():
+    bigram70[key] = bigram70[key] / bi70total
+
+for key in trigram70.keys():
+    trigram70[key] = trigram70[key] / tri70total
+
+# do the stuff on test sets
+
+
+# close the file
+f.close()
---
+++ twitterinstagram.py
@@ -0,0 +1,125 @@
+# NLP Assignment 1, Due 9/25/17
+# Takes in a text file containing potential twitter handles and instagram names,
+# then classifies them using RegExes based on length and character content.
+# @authors Dan Brodkin, Kerim Celik
+
+import re
+
+    #open the file containing our data:
+f = open("Assignment1_InputFile.txt")
+print(f)
+
+    # set up regexes to classify each line's data based on its content
+regex1 = re.compile("@[A-Za-z0-9_]{1,15}\s,\s(TI|I|N)$")
+regex2 = re.compile(".*([Tt][Ww][Ii][Tt]{2}[Ee][Rr]).*")
+regex3 = re.compile("@.*([Aa][Dd][Mm][Ii][Nn]).*")
+regex4 = re.compile("@[A-Za-z0-9_.]{1,30}\s,\s(TI|I|N)$")
+regex5 = re.compile(".*\sTI$")
+regex6 = re.compile(".*\sI$")
+
+    #set up the arrays and counters we'll be using to sort data
+arr = []
+types = []
+i = 0
+q = 0
+
+    # loop through each line to apply the regexes
+for (lin in f):
+    a = regex5.match(lin)
+    b = regex6.match(lin)
+    w = regex4.match(lin)
+    x = regex1.match(lin)
+    y = regex2.match(lin)
+    z = regex3.match(lin)
+
+    # sort the lines based on their content:
+        # if they contain only legal characters, say they can be both
+    if (a != None):
+        types.append(0)
+        # if twitter is ruled out, but insta is possible:
+    elif (b != None):
+        types.append(1)
+        # if they fail at both:
+    else:
+        types.append(2)
+    #try regex4; if it fails, this is neither
+    #try regexes 1-3; if they work, then it's twitter
+    if w == None:
+        arr.append(2)
+        #print("This can't be an instagram or a twitter handle.")
+    elif (x != None) and (y == None) and (z == None):
+        arr.append(0)
+        #print("This can be either.")
+    else:
+        arr.append(1)
+        #print("This can only be an instagram.")
+    i += 1
+
+both = 0
+justInstas = 0
+neither = 0
+
+for n in range(len(types)):
+    if types[n] == 0:                #adds to both total if both
+        both += 1
+    elif types[n] == 1:              #adds to insta total if it can only be that
+        justInstas += 1
+    elif types[n] == 2:               #adds to the 'neither total'
+        neither += 1
+
+
+#PRECISION = number instas identified in [arr] vs their identity in [types]
+#RECALL = number instas identified in [arr] vs total number of instas in [types]
+
+matchBoth = 0
+matchInsta = 0
+matchNeith = 0
+countBoth = 0
+countInsta = 0
+countNeith = 0
+
+for n in range(len(arr)):
+    if (arr[n] == 0):
+        countBoth += 1
+        if (types[n] == arr[n]):
+            matchBoth += 1
+        elif (types[n] == 1):
+            print("Confusion: handle " + str(n) + " was identified as Twitter and Instagram handle instead of just an Instagram handle!")
+        else:
+            print("Confusion: handle " + str(n) + " was identified as Twitter and Instagram handle instead of neither handle type!")
+    elif (arr[n] == 1):
+        countInsta += 1
+        if (types[n] == arr[n]):
+            matchInsta += 1
+        elif (types[n] == 0):
+            print("Confusion: handle " + str(n) + " was identified as just an Instagram handle instead of both a Twitter and Instagram handle!")
+        else:
+            print("Confusion: handle " + str(n) + " was identified as just an Instagram handle instead of neither handle type!")
+    else:
+        countNeith += 1
+        if (types[n] == arr[n]):
+            matchNeith += 1
+        elif (types[n] == 1):
+            print("Confusion: handle " + str(n) + " was identified as neither handle instead of just an Instagram handle!")
+        else:
+            print("Confusion: handle " + str(n) + " was identified as neither handle instead of both a Twitter and Instagram handle!")
+
+prec1 = matchBoth/both
+prec2 = matchInsta/justInstas
+prec3 = matchNeith/neither
+reca1 = countBoth/both
+reca2 = countInsta/justInstas
+reca3 = countNeith/neither
+f1 = (2 * prec1 * reca1) / (prec1 + reca1)
+f2 = (2 * prec2 * reca2) / (prec2 + reca2)
+f3 = (2 * prec3 * reca3) / (prec3 + reca3)
+
+print("Precision (both) = " + str(prec1))
+print("Precision (just Instagram) = " + str(prec2))
+print("Precision (neither) = " + str(prec3))
+print("Recall (both) = " + str(reca1))
+print("Recall (just Instagram) = " + str(reca2))
+print("Recall (neither) = " + str(reca3))
+print("F1 value (both) = " + str(f1))
+print("F1 value (just Instagram) = " + str(f2))
+print("F1 value (neither) = " + str(f3))
