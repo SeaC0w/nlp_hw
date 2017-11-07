6ce4b613782b1033331af9dd44752698bcf77664
seac0w <kerim.celik@yahoo.com>
2017-11-06 18:37:26 -0600

    got tstemmer working, now checks vowel harmony of words

--- dansucks.py
+++ dansucks.py
@@ -1,5 +1,60 @@
 import snowballstemmer
-# from TurkishStemmer import TurkishStemmer
+from TurkishStemmer import HasFrontness
+from TurkishStemmer import HasRoundness
+from TurkishStemmer import Vowels
+import re
 stemmer = snowballstemmer.stemmer('turkish')
-print(stemmer.stemWords("Genç çocuklar".split()))
-print("çocuklar".split('çoc')[0] + '-' + "çocuklar".split('çoc')[1])
+# print(stemmer.stemWords("Genç çocuklar".split()))
+# print(HasFrontness('o', 'u'))
+# print(HasRoundness('o', 'u'))
+# print("çocuklar".split('çoc')[0] + '-' + "çocuklar".split('çoc')[1])
+
+def checkHarmony(word):
+    vowels = Vowels(word)
+    harm = []
+    for i in range(len(vowels) - 1):
+        ro = HasRoundness(vowels[i], vowels[i+1])
+        fr = HasFrontness(vowels[i], vowels[i+1])
+        if ro and fr:
+            harm.append(0)
+        elif ((ro and not fr) or (fr and not ro)):
+            harm.append(1)
+        else:
+            harm.append(2)
+    if (len(harm) == 0):
+        check = 0
+    else:
+        check = max(harm)
+    if check == 0:
+        print(word + " has perfect vowel harmony")
+    elif check == 1:
+        print(word + " has partial vowel harmony")
+    else:
+        print(word + " breaks vowel harmony")
+
+
+f = open('scrubbed.txt')
+turkishCorpus = f.read()
+stemmedWords = stemmer.stemWords(turkishCorpus.split())
+count = 0
+toPrint = []
+while count < 10:
+    count+=1
+    toPrint.append(stemmedWords[count])
+
+# for word in toPrint:
+#     word = re.sub('ı', '#', word)
+#     word = re.sub('ş', '0', word)
+#     word = re.sub('ç', '1', word)
+#     word = re.sub('ö', '2', word)
+#     word = re.sub('ğ', '3', word)
+#     word = re.sub('ü', '4', word)
+#     word = re.sub('Ğ', '5', word)
+#     word = re.sub('Ü', '6', word)
+#     word = re.sub('İ', '7', word)
+#     word = re.sub('Ö', '8', word)
+#     word = re.sub('Ş', '9', word)
+#     word = re.sub('Ç', '@', word)
+
+print(toPrint)
+[checkHarmony(w) for w in toPrint]
--- hw2_nlp/instal.py
+++ hw2_nlp/instal.py
@@ -5,10 +5,16 @@ from tabulate import tabulate
 from nltk.util import ngrams
 from nltk.util import bigrams
 from nltk.util import trigrams
+import sys
 
+# creates a tokenizer to tokenize by sentence
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+# comparison function used in sorting table of word occurences
 def cmp_items(a, b):
     return a[1] - b[1]
 
+# creates the three ngram dictionaries for a train/test split
 def ngram_maker(sentences):
     unigram_dict = dict()
     bigram_dict = dict()
@@ -41,6 +47,8 @@ def ngram_maker(sentences):
         trigs = list(ngrams(nltk.word_tokenize(sent),3))
         if (trigs == []):
             continue
+        # this line of code somehow causes the function to not read
+        # the last sentence in the input file
         if ('START', 'START', trigs[0][0]) not in trigram_dict.keys():
             trigram_dict[('START', 'START', trigs[0][0])] = 1
         else:
@@ -77,6 +85,8 @@ def ngram_maker(sentences):
         trigram_dict[key] = trigram_dict[key] / tri_total
     return ((unigram_dict, bigram_dict, trigram_dict), (uni_total, bi_total, tri_total))
 
+# evaluates a sentence based on a train/test dictionary
+# separate functions for each type of ngram
 def eval_sent_uni(model_dict, sentence):
     prob = 1.0
     for gram in list(ngrams(nltk.word_tokenize(sentence), 1)):
@@ -119,6 +129,7 @@ def eval_sent_tri(model_dict, sentence):
     return prob
 
 # argument d is the dictionary of probabilities created using a train set
+# count is the total number of ngrams in the train set
 def get_unigram_perplexity(sentences, d, count):
     perplex = 0
     for sentence in sentences:
@@ -140,142 +151,191 @@ def get_trigram_perplexity(sentences, d, count):
     perplex = perplex ** (-1 / count)
     return perplex
 
-# creates a tokenizer to tokenize by sentence
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+# data scrubbing function
+def scrub(to_scrub):
+    to_scrub = re.sub('Dr.', 'Dr', to_scrub)
+    to_scrub = re.sub('Mr.', 'Mr', to_scrub)
+    to_scrub = re.sub('Mrs.', 'Mrs', to_scrub)
+    to_scrub = re.sub('Ms.', 'Ms', to_scrub)
+    to_scrub = to_scrub.lower()
+    to_scrub = re.sub('\?', '.', to_scrub)
+    to_scrub = re.sub('!', '.', to_scrub)
+    to_scrub = re.sub("'", "", to_scrub)
+    to_scrub = re.sub('"', '', to_scrub)
+    to_scrub = re.sub('\(', '', to_scrub)
+    to_scrub = re.sub('\)', '', to_scrub)
+    to_scrub = re.sub(',', '', to_scrub)
+    to_scrub = re.sub(';', '', to_scrub)
+    to_scrub = re.sub(':', '', to_scrub)
+    to_scrub = re.sub('-', '', to_scrub)
+    return to_scrub
 
-f = open("HW2_InputFile.txt", 'r')
-fil = f.read()
+def train_test_evaluate(train_test, inp):
+    f = open(train_test, 'r')
+    fil = f.read()
+    fil = scrub(fil)
 
-# removes the period from title prefixes to prevent incorrect sentence splits
+    # get sentence tokens and word tokens
+    sent_tok = tokenizer.tokenize(fil)
+    tokens2 = nltk.word_tokenize(fil)
 
-fil = re.sub('Dr.', 'Dr', fil)
-fil = re.sub('Mr.', 'Mr', fil)
-fil = re.sub('Mrs.', 'Mrs', fil)
-fil = re.sub('Ms.', 'Ms', fil)
-fil = fil.lower()
-fil = re.sub('\?', '.', fil)
-fil = re.sub('!', '.', fil)
-fil = re.sub("'", "", fil)
-fil = re.sub('"', '', fil)
-fil = re.sub('\(', '', fil)
-fil = re.sub('\)', '', fil)
-fil = re.sub(',', '', fil)
-fil = re.sub(';', '', fil)
-fil = re.sub(':', '', fil)
-fil = re.sub('-', '', fil)
+    # tell the user the number of sentences and words in the file
+    num_sent = len(sent_tok)
+    num_word = 0
+    tokens = []
+    for wrd in tokens2:
+        if wrd == '.':
+            continue
+        else:
+            tokens.append(wrd)
+            num_word += 1
 
-# get sentence tokens and word tokens
-sent_tok = tokenizer.tokenize(fil)
-tokens = nltk.word_tokenize(fil)
+    print('Number of sentences in file: ' + str(num_sent))
+    print('Number of words in file: ' + str(num_word))
 
-# tell the user the number of sentences and words in the file
-num_sent = len(sent_tok)
-num_word = 0
-for wrd in tokens:
-    if wrd in '.-':
-        continue
+    # sort the words in file by number of occurences
+    # then print as a table
+    added = []
+    tabl = []
+    for item in tokens:
+        if item not in added:
+            tabl.append([item, 1])
+            added.append(item)
+        else:
+            for row in tabl:
+                if row[0] == item:
+                    row[1] += 1
+
+    # sorts the table
+    to_sort_table = [(y,x) for (x, y) in tabl]
+    to_sort_table.sort()
+    tabl = [(y,x) for (x, y) in to_sort_table]
+    tabl.reverse()
+
+    #prints the table using tabulate module
+    print(tabulate(tabl, headers=['Word', 'Occurences']))
+
+    # split the corpus into 90/10, 80/20, and 70/30 train/test splits
+    sent90 = sent_tok[0:math.floor(0.9*len(sent_tok))]
+    test10 = sent_tok[math.floor(0.9*len(sent_tok)):-1]
+    sent80 = sent_tok[0:math.floor(0.8*len(sent_tok))]
+    test20 = sent_tok[math.floor(0.8*len(sent_tok)):-1]
+    sent70 = sent_tok[0:math.floor(0.7*len(sent_tok))]
+    test30 = sent_tok[math.floor(0.7*len(sent_tok)):-1]
+
+    # creates dictionaries to store the n-grams with their associated occurences
+    # no need to create sentence end tag for bigram and trigram models as
+    # the period of the sentences already acts as a sentence end tag
+    tup = ngram_maker(sent90)
+    unigram90 = tup[0][0]
+    bigram90 = tup[0][1]
+    trigram90 = tup[0][2]
+    uni90count = tup[1][0]
+    bi90count = tup[1][1]
+    tri90count = tup[1][2]
+
+    tup = ngram_maker(sent80)
+    unigram80 = tup[0][0]
+    bigram80 = tup[0][1]
+    trigram80 = tup[0][2]
+    uni80count = tup[1][0]
+    bi80count = tup[1][1]
+    tri80count = tup[1][2]
+
+    tup = ngram_maker(sent70)
+    unigram70 = tup[0][0]
+    bigram70 = tup[0][1]
+    trigram70 = tup[0][2]
+    uni70count = tup[1][0]
+    bi70count = tup[1][1]
+    tri70count = tup[1][2]
+
+    # get the overall preplexities for each train/test split for each type of ngram
+    u90 = get_unigram_perplexity(test10, unigram90, uni90count)
+    u80 = get_unigram_perplexity(test20, unigram80, uni80count)
+    u70 = get_unigram_perplexity(test30, unigram70, uni70count)
+    b90 = get_bigram_perplexity(test10, bigram90, bi90count)
+    b80 = get_bigram_perplexity(test20, bigram80, bi80count)
+    b70 = get_bigram_perplexity(test30, bigram70, bi70count)
+    t90 = get_trigram_perplexity(test10, trigram90, tri90count)
+    t80 = get_trigram_perplexity(test20, trigram80, tri80count)
+    t70 = get_trigram_perplexity(test30, trigram70, tri70count)
+
+    tabl2 = [['Test10 unigram perplexity: ', u90], ['Test20 unigram perplexity: ', u80],
+            ['Test30 unigram perplexity: ', u70], ['Test10 bigram perplexity: ', b90],
+            ['Test20 bigram perplexity: ', b80], ['Test30 bigram perplexity: ', b70],
+            ['Test10 trigram perplexity: ', t90], ['Test20 trigram perplexity: ', t80],
+            ['Test30 trigram perplexity: ', t70]]
+    print('\n')
+    print(tabulate(tabl2, headers=['Test set and n-gram type', 'Perplexity Value']))
+
+    # determine the best train/test split for each model
+    uni_model = -1
+    bi_model = -1
+    tri_model = -1
+    if (u90 <= u80) and (u90 <= u70):
+        uni_model = 0
+    elif (u80 <= u70):
+        uni_model = 1
     else:
-        num_word += 1
-
-print('Number of sentences in file: ' + str(num_sent))
-print('Number of words in file: ' + str(num_word))
-
-# sort the words in file by number of occurences
-# then print as a table
-added = []
-tabl = []
-for item in tokens:
-    if item not in added:
-        tabl.append([item, 1])
-        added.append(item)
+        uni_model = 2
+    if (b90 <= b80) and (b90 <= b70):
+        bi_model = 0
+    elif (b80 <= b70):
+        bi_model = 1
     else:
-        for row in tabl:
-            if row[0] == item:
-                row[1] += 1
+        bi_model = 2
+    if (t90 <= t80) and (t90 <= t70):
+        tri_model = 0
+    elif (t80 <= t70):
+        tri_model = 1
+    else:
+        tri_model = 2
 
-to_sort_table = [(y,x) for (x, y) in tabl]
-to_sort_table.sort()
-tabl = [(y,x) for (x, y) in to_sort_table]
-tabl.reverse()
-print(tabulate(tabl, headers=['Word', 'Occurences']))
+    # open the file containing sentences to evaluate with the
+    # trained and tested models
+    f2 = open(inp, 'r')
+    fil2 = f2.read()
+    fil2 = scrub(fil2)
 
-# split the corpus into 90/10, 80/20, and 70/30 train/test splits
-sent90 = sent_tok[0:math.floor(0.9*len(sent_tok))]
-test10 = sent_tok[math.floor(0.9*len(sent_tok)):-1]
-sent80 = sent_tok[0:math.floor(0.8*len(sent_tok))]
-test20 = sent_tok[math.floor(0.8*len(sent_tok)):-1]
-sent70 = sent_tok[0:math.floor(0.7*len(sent_tok))]
-test30 = sent_tok[math.floor(0.7*len(sent_tok)):-1]
+    tabl3 = []
+    to_eval = tokenizer.tokenize(fil2)
+    c = 0
+    for sentence in to_eval:
+        c += 1
+        if uni_model == 0:
+            entry = ["Sentence " + str(c) + " best unigram MLE:", eval_sent_uni(unigram90, sentence)]
+        elif uni_model == 1:
+            entry = ["Sentence " + str(c) + " best unigram MLE:", eval_sent_uni(unigram80, sentence)]
+        else:
+            entry = ["Sentence " + str(c) + " best unigram MLE:", eval_sent_uni(unigram70, sentence)]
+        if bi_model == 0:
+            entry1 = ["Sentence " + str(c) + " best bigram MLE:", eval_sent_bi(bigram90, sentence)]
+        elif bi_model == 1:
+            entry1 = ["Sentence " + str(c) + " best bigram MLE:", eval_sent_bi(bigram80, sentence)]
+        else:
+            entry1 = ["Sentence " + str(c) + " best bigram MLE:", eval_sent_bi(bigram70, sentence)]
+        if tri_model == 0:
+            entry2 = ["Sentence " + str(c) + " best trigram MLE:", eval_sent_tri(trigram90, sentence)]
+        elif tri_model == 1:
+            entry2 = ["Sentence " + str(c) + " best trigram MLE:", eval_sent_tri(trigram80, sentence)]
+        else:
+            entry2 = ["Sentence " + str(c) + " best trigram MLE:", eval_sent_tri(trigram70, sentence)]
+        tabl3.append(entry)
+        tabl3.append(entry1)
+        tabl3.append(entry2)
 
-# creates dictionaries to store the pos_tag n-grams with
-# their associated occurences
-# no need to create sentence end tag for bigram and trigram models as
-# the period of the sentences already acts as a sentence end tag
-tup = ngram_maker(sent90)
-unigram90 = tup[0][0]
-bigram90 = tup[0][1]
-trigram90 = tup[0][2]
-uni90count = tup[1][0]
-bi90count = tup[1][1]
-tri90count = tup[1][2]
+    print('\n')
+    print(tabulate(tabl3, headers=['N-gram type', 'Sentence Probability']))
+    # close the files
+    f2.close
+    f.close()
 
-tup = ngram_maker(sent80)
-unigram80 = tup[0][0]
-bigram80 = tup[0][1]
-trigram80 = tup[0][2]
-uni80count = tup[1][0]
-bi80count = tup[1][1]
-tri80count = tup[1][2]
+def main():
+    if (len(sys.argv) != 3):
+        print("Incorrect number of command line arguments: requires script name, train/test file name, and input file name")
+    else:
+        train_test_evaluate(sys.argv[1], sys.argv[2])
 
-tup = ngram_maker(sent70)
-unigram70 = tup[0][0]
-bigram70 = tup[0][1]
-trigram70 = tup[0][2]
-uni70count = tup[1][0]
-bi70count = tup[1][1]
-tri70count = tup[1][2]
-
-# get the overall preplexities for each train/test split for each type of ngram
-u90 = get_unigram_perplexity(test10, unigram90, uni90count)
-u80 = get_unigram_perplexity(test20, unigram80, uni80count)
-u70 = get_unigram_perplexity(test30, unigram70, uni70count)
-b90 = get_bigram_perplexity(test10, bigram90, bi90count)
-b80 = get_bigram_perplexity(test20, bigram80, bi80count)
-b70 = get_bigram_perplexity(test30, bigram70, bi70count)
-t90 = get_trigram_perplexity(test10, trigram90, tri90count)
-t80 = get_trigram_perplexity(test20, trigram80, tri80count)
-t70 = get_trigram_perplexity(test30, trigram70, tri70count)
-
-tabl2 = [['Test10 unigram perplexity: ', u90], ['Test20 unigram perplexity: ', u80],
-        ['Test30 unigram perplexity: ', u70], ['Test10 bigram perplexity: ', b90],
-        ['Test20 bigram perplexity: ', b80], ['Test30 bigram perplexity: ', b70],
-        ['Test10 trigram perplexity: ', t90], ['Test20 trigram perplexity: ', t80],
-        ['Test30 trigram perplexity: ', t70]]
-print('\n')
-print(tabulate(tabl2, headers=['Test set and n-gram type', 'Perplexity Value']))
-
-# determine the best train/test split for each model
-uni_model = -1
-bi_model = -1
-tri_model = -1
-if (u90 <= u80) and (u90 <= u70):
-    uni_model = 0
-elif (u80 <= u70):
-    uni_model = 1
-else:
-    uni_model = 2
-if (b90 <= b80) and (b90 <= b70):
-    bi_model = 0
-elif (b80 <= b70):
-    bi_model = 1
-else:
-    bi_model = 2
-if (t90 <= t80) and (t90 <= t70):
-    tri_model = 0
-elif (t80 <= t70):
-    tri_model = 1
-else:
-    tri_model = 2
-
-# close the file
-f.close()
+if __name__ == "__main__":
+    main()
