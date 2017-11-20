a3532b20b85264632b27526e887b5846c7d77241
seac0w <kerim.celik@yahoo.com>
2017-11-20 12:29:12 -0600

    dan please test

---
+++ test.py
@@ -0,0 +1,25 @@
+import snowballstemmer
+from TurkishStemmer import HasFrontness
+from TurkishStemmer import HasRoundness
+from TurkishStemmer import Vowels
+import sys
+import codecs
+# print(HasFrontness('o', 'i') or HasRoundness('o', 'i'))
+# d = {'a' : 0, 'b' : 0, 'c' : 1}
+# x = list(d.items())
+# y = []
+# for item in x:
+#     if (  item[1] == 0):
+#         y.append(item[0])
+#
+# print(y)
+
+print('ı'.upper())
+print('ü'.upper())
+print('ö'.upper())
+print('İ'.lower())
+print('I'.lower())
+print('Ö'.lower())
+# print(HasFrontness('I', 'ı'))
+# print(HasRoundness('I', 'ı'))
+# print("hi                 alex".split(" "))
